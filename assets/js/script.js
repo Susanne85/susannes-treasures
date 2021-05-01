29edc31f869f68395a105bfc73b901ee524b068f
Susanne Bilney <xkqk2010@gmail.com>
2021-05-01 22:52:09 +0800

    Improved formatting and create a new page in a links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
 const hero = document.getElementById('hero');
-
-
+ 
 hero.addEventListener('mouseenter', function(event){
-
-
     event.currentTarget.classList.add('old-pic');
     event.currentTarget.classList.remove('new-pic');
-
 })
 
 hero.addEventListener('mouseleave', function(event){
-
-
     event.currentTarget.classList.add('new-pic');
     event.currentTarget.classList.remove('old-pic');
-
 })
